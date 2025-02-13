fd4e7851230fbe59f017f33c78ac14bef22bdb56
kantonirina02 <kantonirina@outlook.fr>
2025-02-13 19:31:11 +0100

    form Avis resolution pbm habitat

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,7 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/services", "Services", "/pages/services.html"),
+    new Route("/habitats", "Habitats", "/pages/habitats.html")
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
